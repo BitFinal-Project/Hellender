37008cbdba0d2675c2b8f90dd02e420b233a6374
gus-123 <65222175+gus-123@users.noreply.github.com>
2023-12-28 15:18:25 +0900

    Add files via upload

--- spring_17/src/main/java/com/example/spring_17/oauth/controller/MemberController.java
+++ spring_17/src/main/java/com/example/spring_17/oauth/controller/MemberController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000")
-
 @RestController
 @AllArgsConstructor
 @RequestMapping("bit/members")
@@ -41,30 +40,32 @@ public class MemberController {
     }
 
     // Build Update Member REST API
-    @PutMapping("{Uid}")
     // http://localhost:8080/bit/members/1
+    @PutMapping("{Uid}")
     public ResponseEntity<Member> updateMember(@PathVariable("Uid") Integer memberUid, @RequestBody Member member) {
         member.setUid(memberUid);
         Member updateMember = memberService.updateMember(member);
         return new ResponseEntity<>(updateMember, HttpStatus.OK);
     }
 
+    // http://localhost:8080/bit/members/cal/1
     @PutMapping("/cal/{Uid}")
-    // http://localhost:8080/bit/members/1
     public ResponseEntity<Member> calupdateMember(@PathVariable("Uid") Integer memberUid, @RequestBody Member member) {
         member.setUid(memberUid);
         Member calupdateMember = memberService.calupdateMember(member);
         return new ResponseEntity<>(calupdateMember, HttpStatus.OK);
     }
 
+    // http://localhost:8080/bit/members/weight/1
     @PutMapping("/weight/{Uid}")
-    // http://localhost:8080/bit/members/1
     public ResponseEntity<Member> weightupdateMember(@PathVariable("Uid") Integer memberUid, @RequestBody Member member) {
         member.setUid(memberUid);
         Member weightupdateMember = memberService.weightupdateMember(member);
         return new ResponseEntity<>(weightupdateMember, HttpStatus.OK);
     }
 
+    // Build Delete Member REST API
+    // http://localhost:8080/bit/members/1
     @DeleteMapping("{Uid}")
     public ResponseEntity<String> deleteMember(@PathVariable("Uid") Integer memberUId) {
         memberService.deleteMember(memberUId);
